7a2d813a99d77f9e13e44a41662a679538887ffb
Ubuntu <ubuntu@ip-172-31-20-169.eu-west-1.compute.internal>
2019-07-03 20:32:46 +0000

    Created a menu to include Add, Find, Edit & Delete records in python file & update MongoDB database

--- mongo_project.py
+++ mongo_project.py
@@ -22,18 +22,102 @@ def show_menu():
     
     option = input("Enter Option: ") # New variable called option
     return option
+
+def get_record(): # To locate record by only using first &/or last name only # **Helper Function**
+    print("")
+    first = input("Enter first name > ")
+    last = input("Enter last name > ")
     
+    try:
+        doc = coll.find_one({'first': first.lower(), 'last': last.lower()})
+        
+    except:
+        print("Error accessing the database")
+        
+    if not doc:
+        print("Error! No results found") # To cover scenario where search renders zero result.
+        
+    return doc # Returns either a searched record or no record.
+
+def add_record():
+    print("")
+    first = input("Enter first name > ")
+    last = input("Enter last name > ")
+    dob = input("Enter date of birth > ")
+    gender = input("Enter gender > ")
+    hair_colour = input("Enter hair colour > ")
+    occupation = input("Enter occupation > ")
+    nationality = input("Enter nationality > ")
+    
+    new_doc = {'first': first.lower(), 'last': last.lower(), 'dob': dob, 'gender': gender, 'hair_colour': hair_colour, 'occupation': occupation, 'nationality': nationality} # Dictionary built for database
+    
+    try:
+        coll.insert(new_doc) # True statement that allows document to be inserted
+        print("") # Blank line
+        print("Document inserted")
+    except:
+        print("Error accessing database") # False statement creates an error message
+
+def find_record():
+    doc = get_record() # sourced from helper function get_record
+    if doc:
+        print("")
+        for k,v in doc.items(): # k = key, v = value
+            if k != "_id": # We dont want the Mongo ID to be called here for security purposes.
+                print(k.capitalize() + ": " + v.capitalize()) # 1st letter of data field to change to uppercase. 
+
+def edit_record():
+    doc = get_record()
+    if doc:
+        update_doc={}
+        print("")
+        for k, v in doc.items():
+            if k != "_id":
+                update_doc[k] = input(k.capitalize() + " [" + v + "] > ") 
+                # generates a prompt to show the key and what [value has been set to]
+                if update_doc[k] == "": # if no changes made to word, then data base preseves original value 
+                    update_doc[k] = v
+        try:
+            coll.update_one(doc, {'$set': update_doc}) # Updates the document against the parameters set by the Dictionary
+            print("")
+            print("Document updated!")
+            
+        except:
+            print("Error accessing the database")
+
+def delete_record():
+    doc = get_record() # sourced from helper function get_record
+    if doc:
+        print("")
+        for k, v in doc.items(): # k = key, v = value
+            if k != "_id": # We dont want the Mongo ID to be called here for security purposes.
+                print(k.capitalize() + ": " + v.capitalize()) # 1st letter of data field to change to uppercase.
+                
+                print("")
+                confirmation = input("Is this the document you want to delete?\nY or N > ") # Check with user on deletion accuracy
+                print("")
+                
+                if confirmation.lower() == 'y':
+                    try:
+                        coll.remove(doc)
+                        print("Document deleted!") # If User selected 'Y', then "Document deleted!"
+                    except:
+                        print("Error accessing the database")
+                    
+                else:
+                    print("Document not deleted!") # Refers to User selecting 'N'
+
 def main_loop(): # Loop ensures the user is returned to the menu
     while True: # The while True loop will ensure an indefinite loop
         option = show_menu() # Result of the 'show_menu' stored under 'option' variable
         if option == "1":
-            print("You have selected Option 1")
+            add_record()
         elif option == "2":
-            print("You have selected Option 2")
+            find_record()
         elif option == "3":
-            print("You have selected Option 3")
+            edit_record()
         elif option == "4":
-            print("You have selected Option 4")
+            delete_record()
         elif option == "5":
             conn.close() # Maps to connection (conn) definition on row 44  
             break
